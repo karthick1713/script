5b4c6be23ca6d6901336417debbb2c59d56edee0
karthick grl <102960138+karthick1713@users.noreply.github.com>
2023-10-03 12:50:27 +0530

    Add files via upload

---
+++ TesterSinkMode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VseriesControllerLibrary_V1;
+
+namespace Scripts
+{
+    public class TesterSinkMode
+    {
+        public async void MultiRunScript(Dictionary<string, GRLDeviceList> deviceList, PortID portID = PortID.Port1, int delay = 500)
+        {
+
+
+            await Task.Run(() => Parallel.ForEach(deviceList, port =>
+            {
+                if (GrlVPdApiLib.Instance.Set_Controller_Mode(ControllerMode.Sink, port.Value.IPAddress))
+                {
+                    Console.WriteLine("Controller set to Sink mode SuccessFully", port.Value.IPAddress);
+
+                }
+                else
+                {
+                    Console.WriteLine("Controller set to Sink mode is failed ", port.Value.IPAddress);
+
+                }
+            }));
+            await Task.Run(() => Parallel.ForEach(deviceList, port =>
+            {
+                var sourceCaps = GrlVPdApiLib.Instance.SourceCapabilities(portID, port.Value.IPAddress);
+                if (sourceCaps.PDOlist.Count == 0)
+                {
+                    Console.WriteLine("Please connect the DUT");
+                    return;
+                }
+
+                foreach (var pdo in sourceCaps.PDOlist)
+                {
+
+                    if (pdo.PdoType == PDOSupplyType.FixedSupply)
+                    {
+
+                        if ((PDOIndex)pdo.PDO_Index == PDOIndex.PDO2)
+                        {
+                            Console.WriteLine("----------------------------------------------------------------");
+                            Console.WriteLine($"{DateTime.Now}: Controller- {port.Value.IPAddress} Requesting \nPDO Index-" + (PDOIndex)pdo.PDO_Index + "\nVoltage -" + pdo.Voltage + "\nCurrent - " + pdo.Current + "\nMax Operating Current - " + pdo.Current);
+                            Console.WriteLine("----------------------------------------------------------------");
+
+                            GrlVPdApiLib.Instance.RequestPDO(portID, (PDOIndex)pdo.PDO_Index, pdo.Current, pdo.Current, port.Value.IPAddress);
+
+                            GrlVPdApiLib.Instance.SetVbusCurrent(pdo.Current, portID, VbusEload.On, VBUSModeConfig.CCMode, port.Value.IPAddress);
+                            Thread.Sleep(1500);
+
+                            var Voltagestatus = GrlVPdApiLib.Instance.GetVBUSData(portID, port.Value.IPAddress);
+                            Console.WriteLine($"{DateTime.Now}: Controller- {port.Value.IPAddress} {Voltagestatus}");
+
+                        }
+
+                    }
+                }
+
+
+
+            }));
+
+
+
+        }
+    }
+}
---
+++ TesterSourceMode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VseriesControllerLibrary_V1;
+
+namespace Scripts
+{
+    public class TesterSourceMode
+    {
+        public async void RunScript(Dictionary<string, GRLDeviceList> deviceList, PortID portID = PortID.Port1, int delay = 500)
+        {
+            await Task.Run(() => Parallel.ForEach(deviceList, port =>
+            {
+                // This Function will Set Controller MOde as Source
+
+                if (GrlVPdApiLib.Instance.Set_Controller_Mode(ControllerMode.Source, port.Value.IPAddress))
+                {
+                    Console.WriteLine($"{DateTime.Now}:{ port.Value.IPAddress}:Controller set to source mode");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{DateTime.Now}:{ port.Value.IPAddress}:Controller set to source mode is failed ");
+
+                }
+            }));
+
+            await Task.Run(() => Parallel.ForEach(deviceList, port =>
+            {
+
+                ConfigureSourceCapability obj = new ConfigureSourceCapability();
+                obj.AddFixedSupply((uint)(5000), (uint)(3000));
+                obj.AddFixedSupply((uint)(9000), (uint)(3000));
+
+                //This Function will SetSourcecapabliiltiy
+                if (GrlVPdApiLib.Instance.SetSourceCapability(PortID.Port1, obj, port.Value.IPAddress))
+                {
+
+                    Console.WriteLine($"{ port.Value.IPAddress} :Source capabilities configured");
+                }
+                else
+                {
+
+                    Console.WriteLine($"{ port.Value.IPAddress}:Source capabilities configuration failed");
+                }
+                Thread.Sleep(1500);
+
+                //This Function will Get Vbus Data
+                var Voltagestatus = GrlVPdApiLib.Instance.GetVBUSData(portID, port.Value.IPAddress);
+                Console.WriteLine($"{DateTime.Now}: Controller- {port.Value.IPAddress} {Voltagestatus}");
+
+            }));
+        }
+    }
+}
